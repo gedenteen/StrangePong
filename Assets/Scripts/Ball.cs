df71c6a8dd85d45ff96ff15d80f46af7e51734e5
Razumov Dmitry <Razumow.dmitrij@yandex.ru>
2023-06-02 11:31:48 +0600

    Add rotation of the ball (need to finish)

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,5 +1,5 @@
+//using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
@@ -7,6 +7,9 @@ public class Ball : MonoBehaviour
     public Rigidbody2D rb2d;
     public float maxInitialAngle = 0.67f;
     public float moveSpeed = 1f;
+
+    public float rotationDuration = 1f; // Duration in seconds
+    public int rotationCount = 3; // Number of rotations
 
     private float startX = 0f;
     public float maxStartY = 4f;
@@ -19,18 +22,55 @@ public class Ball : MonoBehaviour
 
     private void InitialPush()
     {
-        Vector2 dir = Random.value < 0.5f ? Vector2.left : Vector2.right;
-        dir.y = Random.Range(-maxInitialAngle, maxInitialAngle);
+        Vector2 dir = UnityEngine.Random.value < 0.5f ? Vector2.left : Vector2.right;
+        dir.y = UnityEngine.Random.Range(-maxInitialAngle, maxInitialAngle);
         rb2d.velocity = dir * moveSpeed;
     }
 
-    private void ResetBall()
+    IEnumerator ResetBall()
+    {
+        rb2d.velocity = Vector2.zero;
+
+        // position
+        float posY = UnityEngine.Random.Range(-maxStartY, maxStartY);
+        Vector2 pos = new Vector2(startX, posY);
+        transform.position = pos;
+
+        /*float elapsedTime = 0f;
+        Quaternion startRotation = transform.rotation;
+        Quaternion targetRotation = Quaternion.Euler(0f, 0f, 180f); // Replace with desired final rotation
+
+        while (elapsedTime < timeForRotation)
+        {
+            transform.rotation = Quaternion.Lerp(startRotation, targetRotation, elapsedTime / timeForRotation);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }*/
+        for (int i = 0; i < rotationCount; i++)
+        {
+            float elapsedTime = 0f;
+            Quaternion startRotation = transform.rotation;
+            Debug.Log("transform.rotation=" + transform.rotation);
+            Quaternion targetRotation = Quaternion.Euler(0f, 0f, (i%2+1) * 180f);
+
+            while (elapsedTime < rotationDuration)
+            {
+                transform.rotation = Quaternion.Slerp(startRotation, targetRotation, elapsedTime / rotationDuration);
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+        }
+
+        InitialPush();
+    }
+
+    private void ResetBallPart2()
     {
         // direction
         InitialPush();
 
         // position
-        float posY = Random.Range(-maxStartY, maxStartY);
+        float posY = UnityEngine.Random.Range(-maxStartY, maxStartY);
         Vector2 pos = new Vector2(startX, posY);
         transform.position = pos;
     }
@@ -40,7 +80,7 @@ public class Ball : MonoBehaviour
         ScoreZone scoreZone = collision.GetComponent<ScoreZone>();
         if (scoreZone != null)
         {
-            ResetBall();
+            StartCoroutine(ResetBall());
         }
     }
 }
